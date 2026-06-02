5d2c8f553c10227f97ead83e9dd5fe59cd4465c9
SaptarshiMaji <saptarshimaji13@gmail.com>
2026-06-03 00:14:19 +0530

    Add Render database support

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-DATABASE_URI = "postgresql://postgres:12345@localhost:5432/smart_parking"
+import os
+
+DATABASE_URI = os.getenv(
+    "postgresql://smart_parking_user:59gTr8Vvhkdfy3hlFcgACsz1i13g3gd8@dpg-d8fi7s57vvec738cd1rg-a.oregon-postgres.render.com/smart_parking_hdhn"
+)
