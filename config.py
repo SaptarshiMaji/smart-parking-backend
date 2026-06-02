a9dd9e1233822f837aeb83583a298bfb5c2b121b
SaptarshiMaji <saptarshimaji13@gmail.com>
2026-06-03 00:24:46 +0530

    Fix database environment variable

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 DATABASE_URI = os.getenv(
-    "postgresql://smart_parking_user:59gTr8Vvhkdfy3hlFcgACsz1i13g3gd8@dpg-d8fi7s57vvec738cd1rg-a.oregon-postgres.render.com/smart_parking_hdhn"
+    "DATABASE_URL"
 )
